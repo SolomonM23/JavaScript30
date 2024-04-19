fe94221b785fe40cc73af77b959927bf2e15924a
SolomonM23 <solomonmalone@yahoo.com>
2024-04-19 14:57:37 -0400

    progress bar changes video playback location when clicking points in the progressbar

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -40,6 +40,11 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
 
 //3. event listeners
 video.addEventListener('click', togglePlay);
@@ -52,3 +57,5 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
