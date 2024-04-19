aced22cd34efadb4ca5b43456076f9c5e2b91d70
SolomonM23 <solomonmalone@yahoo.com>
2024-04-19 12:27:54 -0400

    added play/pause function, skip buttons function, and event listeners for both functions

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,6 +8,30 @@ const ranges = player.querySelectorAll('.player__slider');
 const skipButtons = player.querySelectorAll('[data-skip]');
 
 //2. build the functions
+function togglePlay() {
+    if (video.paused){
+        video.play();
+    }else {
+        video.pause();
+    }
+}
 
+//change play/pause icon
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+    console.log('Update the button');
+}
+
+function skip(){
+    console.log(this.dataset.skip);
+    video.currentTime += +(this.dataset.skip); //this.dataset.skip converted to a number
+}
 
 //3. event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
